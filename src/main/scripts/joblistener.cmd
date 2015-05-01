31ba81e10eaca5ec4014a98d6b1f58eee2f0f73e
Jens Ritter <jens.ritter@gmail.com>
2015-05-01 06:08:18 +0200

    working .cmd file :)

---
+++ src/main/scripts/joblistener.cmd
@@ -0,0 +1,29 @@
+@echo off
+REM needed to overcome weird loop behavior
+REM in conjunction with variable expansion
+SETLOCAL enabledelayedexpansion
+
+REM Settings
+set basedir=.
+set libdir=libs
+set configdir=etc
+
+
+REM construct classpath of seperate jars
+set cp=""
+
+FOR %%F IN (%libdir%/*.jar) DO (
+  SET cp=!cp!;%libdir%/%%F%
+)
+
+set java="%JAVA_HOME%\bin\java.exe"
+set config=%confdir%\joblistener.properties
+set logback=%confdir%\logback.xml
+set mainclass=org.jens.test.JobListenerApplication
+
+
+%java% -cp %cp% ^
+    -Dconfig=%config% ^
+    -Dlogback.configurationFile=%logback% ^
+    %mainclass%
+exit /B %ERRORLEVEL%
